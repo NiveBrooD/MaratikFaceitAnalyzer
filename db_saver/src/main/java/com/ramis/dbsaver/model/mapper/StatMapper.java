e437d2bfbb9c2b82b1c6c90d13ae11a855c59674
Ramis <ramis5564629@gmail.com>
2026-01-07 16:02:49 +0300

    commit

--- db_saver/src/main/java/com/ramis/dbsaver/model/mapper/StatMapper.java
+++ db_saver/src/main/java/com/ramis/dbsaver/model/mapper/StatMapper.java
@@ -3,9 +3,11 @@ package com.ramis.dbsaver.model.mapper;
 import com.ramis.dbsaver.model.StatsResponse;
 import com.ramis.dbsaver.model.Stats;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface StatMapper {
 
+    @Mapping(target = "id", ignore = true)
     Stats to(StatsResponse statsResponse);
 }
